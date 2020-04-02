5dc027299cca900499813e67c5edf1d2bd039f10
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-04-01 23:37:15 -0700

    Initial Commit

---
+++ WalkWalkRevolutionTests/WalkWalkRevolutionTests.swift
@@ -0,0 +1,34 @@
+//
+//  WalkWalkRevolutionTests.swift
+//  WalkWalkRevolutionTests
+//
+//  Created by Juan Dominguez  on 4/1/20.
+//  Copyright © 2020 Juan Dominguez and Qiyue Wang. All rights reserved.
+//
+
+import XCTest
+@testable import WalkWalkRevolution
+
+class WalkWalkRevolutionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
